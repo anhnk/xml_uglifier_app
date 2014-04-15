b788fdabf374ae72787037ae6eedbb50110af8a6
Anh Do <dotuanhoanganh@gmail.com>
2014-04-15 23:24:24 +1000

    add API

--- app/controllers/uglify_controller.rb
+++ app/controllers/uglify_controller.rb
@@ -1,8 +1,25 @@
 class UglifyController < ApplicationController
+  skip_before_filter :verify_authenticity_token, if: :json_request?
+
   def index
+    respond_to do |format|
+      format.json { render json: { name: 'Anh Do', status: :ok} }
+      format.html
+    end
   end
 
   def show
-  	@content = XmlUglifier.uglify params[:uglifier][:content]
+    @content = XmlUglifier.uglify params[:uglifier][:content] unless params[:uglifier].nil?
+
+    respond_to do |format|
+      format.json { render json: { uglified_xml: 'Nilo', status: :ok} }
+      format.html
+    end
+  end
+
+  protected
+
+  def json_request?
+	request.format.to_s == '*/*'
   end
 end
